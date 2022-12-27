d539adb16e932136cc5cc789aa9136bab7b1eabd
ACER <np03cs4s210145@heraldcollege.edu.np>
2022-12-27 09:24:51 +0545

    Frontedn Dashboard

--- grocerymanagementapp/src/App.js
+++ grocerymanagementapp/src/App.js
@@ -1,22 +1,19 @@
-import {Route,Routes,Navigate } from 'react-router-dom';
-import Main from './components/Main';
-import Signup from './components/Signup'
-import Login from './components/Login';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
 
-
+import Login from "./components/Login";
 
 function App() {
- // const user = localStorage("token")
+	const user = localStorage.getItem("token");
 
-  return (
-    <Routes>
-      {/* {user && <Route path= "/" exact element={<Main/>}/>} */}
-      <Route path ="/signup"  element ={<Signup/>}/>
-      <Route path ="/login" element ={<Login/>}/> 
-      <Route path ="/"  element ={<Navigate replace to="/login"/>}/>
-    </Routes>
-   
-  );
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
