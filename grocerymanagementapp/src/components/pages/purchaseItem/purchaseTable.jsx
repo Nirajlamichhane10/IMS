de8fb279d8d7ec46123503913835d90c8bb5c353
ACER <np03cs4s210145@heraldcollege.edu.np>
2023-04-15 23:50:05 +0545

    purchase item page

--- grocerymanagementapp/src/components/pages/purchaseItem/purchaseTable.jsx
+++ grocerymanagementapp/src/components/pages/purchaseItem/purchaseTable.jsx
@@ -24,7 +24,12 @@ import ViewColumn from '@mui/icons-material/ViewColumn';
 import { useEffect,useState } from 'react';
 import axios from 'axios';
 import Button from '@mui/material/Button';
-
+import { log } from 'joi-browser';
+import { v4 as uuid } from 'uuid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -59,74 +64,89 @@ const tableIcons = {
       height:"35%",
       margin:"-35px 10px 10px 250px",
     
-    }
+    },
+    buttonprint:{
+      margin:"30px 20px 50px 790px",
+      width:"220px",
+    
+     },
+     button:{
+      margin:"20px 0px 50px 1050px",
+      width:"220px",
+     }
 
 };
 
 export default function PurchasedTable(props) {
   const { useState } = React;
-  const [itemName, setItemName]= React.useState("");
+  const [itemName, setItemName] = React.useState([]);
+ 
   const [quantity, setQuantity]= React.useState(0);
   const [price, setPrice ]= React.useState(0);
   const [total, setTotal ]=React.useState(0);
   const[unitOfItem, setUnitOfItem] =React.useState("");
+  const [items, setItems] = React.useState([{}]);
 
-  const invoiceNumber= React.useState(props.invoiceNumber);
-  const billDate= React.useState(props.billDate);
-  const supplierName= React.useState(props.supplierName);
-
-  const [testObject, setTestObject] = React.useState({
-    
-  });
-
-  const handleChangeItemName =(event)=>{
-    setItemName(event.target.value)
-  };
-
-  const handleChangeQuantity =(event)=>{
-    setQuantity(event.target.value)
-  };
-
-  const handleChangePrice =(event)=>{
-    setPrice(event.target.value)
-  };
-
-  const handleChangeUnitOfItem =(event)=>{
-    setUnitOfItem(event.target.value)
-  };
-
-  const handleChangeTotal =(event)=>{
-    setTotal(event.target.value)
-  };
+  const {invoiceNumber, billDate,supplierName, setInvoiceNumber,setSupplierName,setBillDate,setSelectedSupplier}= props;
   
+
 
 
   const[message, setMessage]= React.useState("");
   const[status, setStatus]= React.useState("");
   const[open, setOpen]= React.useState(false);
 
+
+  // useEffect=()=>{
+  //   fetchItemName();
+  // }
+
+  // reset 
   const reset =()=>{
     
-    setItemName("");
-    setQuantity("");
-    setPrice("");
-    setTotal("");
-    setUnitOfItem("");
-  
+    setInvoiceNumber("");
+    setSupplierName("");
+    setSelectedSupplier("Select supplier");
+    setBillDate(null);
+    setItems([{}]);
+    setData([]);
+
+    const unique_id = uuid();
+  				const small_id = unique_id.slice(0,8);
+				
+				console.log(small_id);
+        setInvoiceNumber(small_id);
+
   }
-
-   
-  const defaultMaterialTheme = createTheme();
-    
-
   
+//   const fetchItemName= async() => {
+//   try{
+//       const res = await axios.get(' http://localhost:5000/addItem/getItem',{itemName});
+//       setItemName(res.data); 
+//   }
+//   catch(e){
+//     console.log(e);
+//   }
+//  }
+   
+  
+  const defaultMaterialTheme = createTheme();
     const [columns, setColumns] = useState([
    
-      { title: 'Item  Name', field: 'itemName', initialEditValue: 'initial edit value' },
+      { title: 'Item  Name', field: 'itemName' , initialEditValue: 'initial edit value' 
+      // render: rowData => (
+      //   // Custom rendering for the 'Name' column
+      //   <div>
+
+      //      </div>
+
+        
+      // ),
+    },
       { title: 'Unit', field: 'unitOfItem', initialEditValue: 'initial edit value' },
-      { title: 'Quantity', field: 'quantity', initialEditValue: 'initial edit value' },
-      { title: 'Price', field: 'price', initialEditValue: 'initial edit value' },
-      { title: 'Total', field: 'total', initialEditValue: 'initial edit value' },
+      { title: 'Quantity', field: 'quantity', initialEditValue: 0 },
+      { title: 'Price', field: 'price', initialEditValue: 0 },
+      { title: 'Total', field: 'total', initialEditValue: 0,disabled:true },
      
       
 
@@ -140,6 +160,24 @@ export default function PurchasedTable(props) {
     ]);
 
 
+  
+    const test = () => {
+      console.log(items)
+    }
+    // const handleOnclick= async ()=>{
+    //   try {
+    //     const resp = await axios.post("http://localhost:5000/purchaseItem/purchase");
+    //     console.log(resp)
+  
+    //   }
+    //   catch(e){
+    //     console.log(e);
+    //   }
+  
+  
+    // }
+
+
 // // saving data 
 // const handleSave = async () => {
 //   try {
@@ -151,12 +189,11 @@ export default function PurchasedTable(props) {
 //     // handle error if needed
 //   }
 // };
-const handleSave = async () => {
-  const items = [{ itemName, quantity, price, total, unitOfItem }];
-  
-      
+const handleOnclick = async () => {    
+
+  items.shift();
   try{
-    const response = await axios.post("http://localhost:5000/purchaseItem/postPurchase",{invoiceNumber,billDate,supplierName,items});
+    const response = await axios.post("http://localhost:5000/purchaseItem/purchase",{invoiceNumber,billDate,supplierName,items});
     console.log(response);
     setMessage("Items purchased successfully");
     setStatus("success");
@@ -182,10 +219,17 @@ const handleSave = async () => {
         columns={columns}
         
         data={data}
-
+// for adding total
         options={{
           search:false,
           paging:false,
+          // showTotal: true,
+          // totalRow: { name: 'Grand Total', total: ()=>{
+          //   let total = 0;
+          //   data.forEach(item => {
+          //     total+= item.quantity * item.price;
+          //   });
+          //   return total;} },
         }}
 
         components={{
@@ -203,9 +247,16 @@ const handleSave = async () => {
           onRowAdd: newData =>
             new Promise((resolve, reject) => {
               setTimeout(() => {
+                const total = newData.price * newData.quantity;
+                newData.total=total;
                 setData([...data, newData]);
+                const tempItem={"itemName":newData.itemName,"unitOfItem":newData.unitOfItem,"quantity":newData.quantity,"price":newData.price,"total":total};
+                setItems([...items, {...tempItem} ]);
+               console.log("Total Data");
+                console.log(newData);
+                console.log(newData.price);
                
-                handleSave(); // save data to backend
+               
                 resolve();
               }, 1000)
             }),
@@ -216,9 +267,8 @@ const handleSave = async () => {
                 const index = oldData.tableData.id;
                 dataUpdate[index] = newData;
                 setData([...dataUpdate]);
-                setTestObject(newData);
-                console.log(newData);
-                handleSave();// save data to backend
+
+                
   
                 resolve();
               }, 1000)
@@ -239,7 +289,14 @@ const handleSave = async () => {
       />
           
       </ThemeProvider>
-     
+      <div style={Styles.button}>
+        <Button onClick={handleOnclick} variant="contained" size="large">
+          SAVE ITEM 
+        </Button>
+      </div>
+      {/* <Button onClick={test} >
+      Test
+        </Button> */}
       </div>
     )
 
