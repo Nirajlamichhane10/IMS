9f3f028d943439c806675b9c6ec8652b237e4355
ACER <np03cs4s210145@heraldcollege.edu.np>
2023-05-13 14:30:07 +0545

    password bcrypt

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+
 
 const userSchema = new mongoose.Schema({
   pin: {type: Number, require: true},
@@ -8,17 +8,6 @@ const userSchema = new mongoose.Schema({
 
 });
  
-userSchema.pre('save', async function (next) {
-  try {
-    if (!this.isModified('password')) {
-      return next();
-    }
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
+
 
 module.exports = mongoose.model("User", userSchema);
